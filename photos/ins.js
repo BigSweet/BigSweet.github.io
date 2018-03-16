558e5dca3e696b060f8c77b15961e9213b10b35c
sunwentao <wentao.sun@yintech.cn>
2018-03-16 16:01:46 +0800

    Site updated: 2018-03-16 16:01:46

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/BigSweet/Blog/tree/master/photos/' + data.link[i] + '.min.jpg';
-          var src = 'https://github.com/BigSweet/Blog/tree/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/BigSweet/Blog/master/photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://raw.githubusercontent.com/BigSweet/Blog/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
